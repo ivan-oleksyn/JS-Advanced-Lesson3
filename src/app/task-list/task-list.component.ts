98a296ae37365439809049fd069e8182e22eb6e1
ivan-oleksyn <vnlksn07@gmail.com>
2022-08-01 21:00:50 +0200

    Adding project 'Task list'

---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+interface ITask{
+  name:string,
+  status:boolean
+}
+
+class Task implements ITask{
+  public name: string;
+  public status: boolean
+  constructor(name: string, status: boolean) {
+    this.name = name;
+    this.status = false;
+    }
+}
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.scss']
+})
+export class TaskListComponent implements OnInit {
+
+  public taskName!: string;
+  public task!: ITask;
+  public tasks: Task[] = [{ 'name': 'HTML5', 'status': true }, { 'name': 'CSS3', 'status': true },
+    { 'name': 'SCSS', 'status': false }, { 'name': 'JavaScript', 'status': false }, { 'name': 'jQuery', 'status': false },
+    { 'name': 'Angular', 'status': false}];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addTask(): void{
+    this.task = {
+      name: this.taskName,
+      status: false
+    }
+    this.tasks.push(this.task);
+    this.taskName = '';
+  }
+
+}
